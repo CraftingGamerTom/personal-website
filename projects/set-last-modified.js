67f833bd6fd2d569f6ed377b7e6050cb33fac6c7
CraftingGamerTom <tcrokicki@gmail.com>
2019-02-17 22:40:02 -0500

    Initial Import from local development

---
+++ projects/set-last-modified.js
@@ -0,0 +1,7 @@
+ /* Script to set the last modified date */
+window.onload = function() {
+	document.lastModified 
+	var value = document.lastModified;
+	console.log(value);
+	document.getElementById("lastModified").innerHTML = "Revised " + value;
+};
