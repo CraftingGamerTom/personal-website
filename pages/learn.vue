9ed3827691371218c5f3336cdd72f2a3823d676d
Thomas Rokicki <tcrokicki@gmail.com>
2021-05-31 23:38:57 -0400

    Placeholder and styling for contact me and learn page

---
+++ pages/learn.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>This is on my <nuxt-link to="/changelog">roadmap</nuxt-link></h1>
+        
+    </div>
+</template>
+
+
+<script>
+export default {
+  scrollToTop: true,
+  meta: {
+    breadcrumbItems: [
+      {
+        text: "Learn",
+        active: true
+      }
+    ]
+  }
+};
+</script>
