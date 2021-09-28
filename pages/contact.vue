cff882516a38235ca1d0017b4f10eccb513f255c
CraftingGamerTom <tcrokicki@gmail.com>
2021-09-27 22:59:16 -0400

    Create contact me form and API

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,27 +1,35 @@
 <template>
   <b-row>
     <b-col cols="12" offset="0" md="6" offset-md="3">
-      <b-container id="index-recent-projects" class="container--1">
-        <h1 style="padding-top:25px;">What's Up?</h1>
+      <b-container v-if="submissionSuccess" class="container--1">
+        <h2>You're Awesome!</h2>
+        <p>I'll get back to you soon</p>
+        <b-button class="container-bottom-btn" @click="onReset()">
+          Reset
+        </b-button>
+      </b-container>
+      <b-container v-else-if="submissionFailure" class="container--1">
+        <h2>Could not process your request.</h2>
+        <p>
+          Try emailing me directly via tcrokicki@gmail.com.
+          Your message:
+        </p>
+        <p>{{ form.message }}</p>
+        <b-button class="container-bottom-btn" type="reset">
+          Reset
+        </b-button>
+      </b-container>
+      <b-container v-else id="index-contact-me" class="container--1">
+        <h1 style="padding-top:25px;">
+          What's Up?
+        </h1>
         <h5 style="padding-top:15px;padding-bottom:40px;">
           Looking to build something together? Let's get started!
         </h5>
 
-        <!-- <div v-if="recentProjects.fetchError" class="text--error">
-              <h3>Error loading recent projects</h3>
-              <p>tttt</p>
-            </div>
-            <b-row v-else>
-              <b-col cols="12" md="6" lg="3" v-for="project in recentProjects.projects" :key="project.id">
-                <b-container class="container--2"> 
-                  <h1>t</h1>
-                </b-container>
-              </b-col>
-            </b-row> -->
-
         <b-row>
           <b-col cols="8" offset="2">
-            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form v-if="show" @submit="onSubmit" @reset="onReset">
               <b-form-group
                 id="input-group-1"
                 label="What should I call you?"
@@ -31,8 +39,7 @@
                   id="input-name"
                   v-model="form.name"
                   placeholder="Your name"
-                  required
-                ></b-form-input>
+                />
               </b-form-group>
 
               <b-form-group
@@ -45,16 +52,14 @@
                   v-model="form.email"
                   type="email"
                   placeholder="Enter email"
-                  required
-                ></b-form-input>
-                <br />
+                />
+                <br>
                 <b-form-input
                   id="input-phone"
                   v-model="form.phone"
                   type="tel"
                   placeholder="Phone Number"
-                  required
-                ></b-form-input>
+                />
               </b-form-group>
 
               <b-form-group
@@ -68,18 +73,23 @@
                   placeholder="Type your message here"
                   rows="3"
                   max-rows="6"
-                ></b-form-textarea>
+                />
               </b-form-group>
-
-              <b-button type="submit" variant="primary">Submit</b-button>
-              <b-button type="reset" variant="danger">Reset</b-button>
+              <b-button class="container-bottom-btn" type="submit">
+                Submit
+              </b-button>
             </b-form>
           </b-col>
         </b-row>
 
-        <b-button style="margin-bottom:15px;" :to="'/projects'"
-          >View All</b-button
-        >
+        <p v-if="errors.length">
+          <b>Please correct the following error(s):</b>
+          <ul style="text-align:left">
+            <li v-for="error in errors" :key="error">
+              {{ error }}
+            </li>
+          </ul>
+        </p>
       </b-container>
     </b-col>
   </b-row>
@@ -88,42 +98,69 @@
 <script>
 // validate the fields before post (like number being the ciorrect number of digits)
 export default {
-  data() {
+  data () {
     return {
+      show: true,
+      submissionSuccess: null,
+      submissionFailure: null,
+      errors: [],
       form: {
-        email: "",
-        name: "",
-        food: null,
-        checked: []
-      },
-      foods: [
-        { text: "Select One", value: null },
-        "Carrots",
-        "Beans",
-        "Tomatoes",
-        "Corn"
-      ],
-      show: true
-    };
+        name: null,
+        email: null,
+        phone: null,
+        message: null
+      }
+    }
   },
   methods: {
-    onSubmit(event) {
-      event.preventDefault();
-      alert(JSON.stringify(this.form));
+    async onSubmit (event) {
+      event.preventDefault()
+      if (this.checkForm()) {
+        const _this = this
+        await this.$axios.post('/api/contact', this.form)
+          .then((response) => {
+            _this.$log.debug(response)
+            _this.submissionSuccess = response.status
+          }).catch((error) => {
+            _this.$log.error(error)
+            _this.submissionFailure = error.status
+          })
+      }
     },
-    onReset(event) {
-      event.preventDefault();
+    onReset () {
       // Reset our form values
-      this.form.email = "";
-      this.form.name = "";
-      this.form.food = null;
-      this.form.checked = [];
+      this.form.email = null
+      this.form.name = null
+      this.form.phone = null
+      this.form.message = null
+      this.errors = []
+      this.submissionSuccess = null
+      this.submissionFailure = null
+
       // Trick to reset/clear native browser form validation state
-      this.show = false;
+      this.show = false
       this.$nextTick(() => {
-        this.show = true;
-      });
+        this.show = true
+      })
+    },
+    checkForm () {
+      this.errors = []
+
+      if (!this.form.name) {
+        this.errors.push('Name is required.')
+      }
+      if (!this.form.email && !this.form.phone) {
+        this.errors.push('Phone number or email must be provided')
+      }
+      if (!this.form.message) {
+        this.errors.push('Provide a message!')
+      }
+
+      if (this.errors.length) {
+        return false
+      }
+      return true
     }
   }
-};
+}
 </script>
